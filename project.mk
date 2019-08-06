3683b8d91f6037663b2571ee2f7ada158404027f
Leon ROUX <contact@heofon.co>
2019-08-06 15:38:24 +0200

    Fixed because Darwin is dumb :o

--- project.mk
+++ project.mk
@@ -2,11 +2,13 @@
 include $(shell dirname $(lastword $(MAKEFILE_LIST)))/core.mk
 
 # Getting dependencies for subsequent libraries
+ifneq ($(shell uname -s),Darwin)
 $(foreach libfile, $(LIBSFILES), \
 	$(eval LIBSDEPS.$(notdir $(libfile)) := \
 		$(addprefix $(dir $(libfile)), \
 			$(shell $(MAKE) -sC $(dir $(libfile)) show-deps)))${\n} \
 )
+endif
 
 # Creating dependency targets
 $(foreach libfile, $(LIBSFILES), \
